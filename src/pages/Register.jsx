0569878417f0c84c85c625aa09a5159cb3202a95
Abhishek Yadav <yabhi594@gmail.com>
2026-02-09 21:47:38 +0530

    ref: not needed the register page text

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,10 @@
 function Register() {
-  return <h2>Register Page</h2>;
+  return (
+    <div className="text-6xl text-center text-blue-600 font-extrabold mt-20">
+      Registration is not needed <br />
+      and not implemented
+    </div>
+  );
 }
 
 export default Register;
